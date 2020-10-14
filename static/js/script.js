eb50852a2d417183d764dcb9854685d8dd6fe505
jess-bennett <jess.bennett@posteo.net>
2020-10-14 12:08:40 +0000

    Added accordion layout to Hive page

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 /* https://stackoverflow.com/questions/21727317/how-to-check-confirm-password-field-in-form-without-reloading-page/21727518 */
+/* Check passwords match */
 $('#password, #confirm_password').on('keyup', function () {
   if ($('#password').val() == $('#confirm_password').val()) {
     $('#passwordMatch').html('Passwords match').css('color', '#888C1B');
@@ -7,15 +8,29 @@ $('#password, #confirm_password').on('keyup', function () {
     stopSubmission();
 });
 
+/* Prevent submission if passwords do not match */
 $("#registrationSubmit").click(function(event) {
     if ($('#password').val() != $('#confirm_password').val()) {
         event.preventDefault();
     }           
 });
 
+/* Hide viewall on page load */
+$(document).ready(function(){
+    $("#category-layout").show();
+    $("#viewall-layout").hide();
+});
+
+/* Function to switch page view on Hive */
  $("#btn-categories").click(function(){
-    $(".category-layout").show();
+    $("#category-layout").show();
+    $("#viewall-layout").hide();
+    $("#btn-categories").addClass("page-selected")
+    $("#btn-viewall").removeClass("page-selected")
   });
   $("#btn-viewall").click(function(){
-    $(".category-layout").hide();
+    $("#viewall-layout").show();
+    $("#category-layout").hide();
+    $("#btn-viewall").addClass("page-selected")
+    $("#btn-categories").removeClass("page-selected")
   });
