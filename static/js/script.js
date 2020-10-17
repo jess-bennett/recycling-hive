b3ada7c7e82830b7507c7c5db2593693bdd0317f
jess-bennett <jess.bennett@posteo.net>
2020-10-17 11:57:27 +0000

    Started JS to select category in dropdown on page change

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,31 @@
 /* https://stackoverflow.com/questions/21727317/how-to-check-confirm-password-field-in-form-without-reloading-page/21727518 */
-/* Check passwords match */
-$('#password, #confirm_password').on('keyup', function () {
-  if ($('#password').val() == $('#confirm_password').val()) {
+/* Check passwords match on Registration page */
+$('#password, #confirm-password').on('keyup', function () {
+  if ($('#password').val() == $('#confirm-password').val()) {
     $('#password-match').html('Passwords match').css('color', '#888C1B');
   } else 
     $('#password-match').html('Passwords do not match').css('color', '#8C0A06');
     stopSubmission();
 });
 
-/* Prevent submission if passwords do not match */
+/* Prevent submission if passwords do not match on Registration page */
 $("#submit-registration").click(function(event) {
-    if ($('#password').val() != $('#confirm_password').val()) {
+    if ($('#password').val() != $('#confirm-password').val()) {
         event.preventDefault();
     }           
 });
 
-/* Hide viewall on page load */
+/* Collapse accordion on page load on Hive category page  */
 $(document).ready(function(){
     $('.collapse').collapse()
 });
 
-
+/* Select category in dropdown on page change */
+$("#select-category").click(function(event) {
+    $("select#itemCategory option").filter(function() {
+                    return $(this).val() == $(this).val(); 
+                }).prop('selected', true);         
+});
 
   /* Change list of recycling items on dropdown change */
   $("#itemCategory").change(function () {
